4662e048b99f125d6ccb076b9fe77bac7f461211
dhtjddls <dhtjddls12@naver.com>
2022-01-26 16:55:36 +0900

    make todolist using react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,37 @@
-import Button from  "./Button";
+import btn from  "./Button.module.css";
 import styles from "./App.module.css"
 import { useState, useEffect } from 'react';
 import { func } from "prop-types";
 
-function Hello(){
-  useEffect(() => {
-    console.log('hi :)')
-    return () => console.log('bye :(')
-  }, [])
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [...currentArray, toDo]);
+  }
+  const onRemove = (index) => {setToDos(toDos.filter((list, todoindex) => index !== todoindex));
+  }
   return <div>
-    {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? 'hide' : 'show'}</button>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type='text' placeholder="Write your to do..."/> 
+      <button>Add To Do</button>
+    </form>
+    <hr />
+    <ul>
+      {toDos.reverse().map((item, index) =>(
+      <li key={index}>
+        {item}
+        <button className={btn.btn} onClick={() => onRemove(index)}>X</button>
+      </li>
+      ))}
+    </ul>
   </div>;
 }
 
