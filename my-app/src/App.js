34f7b7be93763945e982c5b27a0e84ab2ead5f72
dhtjddls <dhtjddls12@naver.com>
2022-01-27 19:10:45 +0900

    make coin state loader

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,35 +4,29 @@ import { useState, useEffect } from 'react';
 import { func } from "prop-types";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos(currentArray => [...currentArray, toDo]);
-  }
-  const onRemove = (index) => {setToDos(toDos.filter((list, todoindex) => index !== todoindex));
-  }
-  return <div>
-    <h1>My To Dos ({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={toDo} type='text' placeholder="Write your to do..."/> 
-      <button>Add To Do</button>
-    </form>
-    <hr />
-    <ul>
-      {toDos.reverse().map((item, index) =>(
-      <li key={index}>
-        {item}
-        <button className={btn.btn} onClick={() => onRemove(index)}>X</button>
-      </li>
-      ))}
-    </ul>
-  </div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+      fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+  <div>
+    <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+    {loading ? <strong>Loading...</strong> : 
+    <select>
+      {coins.map((coin) => 
+      <option>
+        {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+      </option>)}
+    </select>}
+    
+  </div>
+  );
 }
 
 export default App;
